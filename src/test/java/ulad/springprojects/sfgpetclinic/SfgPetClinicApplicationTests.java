05a3bac3ba9e44803e5c39d705f143c600811819
Uladzislau Dubrouski <u.dubrouski@godeltech.com>
2020-10-24 02:09:24 +0300

    Initial setup of spring boot project

---
+++ src/test/java/ulad/springprojects/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ulad.springprojects.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
